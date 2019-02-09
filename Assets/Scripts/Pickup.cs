81a5538407d7517becfc10717caa9706d44baddf
ivanmaslyuk <ivan.maslyuck@live.com>
2019-02-09 11:00:19 +0300

    добавил пустой shootsystem

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickupName
+{
+    Gun,
+    Grenade
+}
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class Pickup : MonoBehaviour {
+
+    [SerializeField] int RemainingAmmo;
+    [SerializeField] PickupName Name;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ShootSystem shootSystem = other.gameObject.GetComponent<ShootSystem>();
+            shootSystem.OnPickupEnter(this);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPickupEnter(Pickup pickup)
+    {
+        // Стандартная реализация.
+        Destroy(pickup.gameObject);
+    }
+}
